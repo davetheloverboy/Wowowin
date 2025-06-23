098bf86bfb4c3bd6b82e2e3c1d089b024855c909
davetheloverboy <70562084+davetheloverboy@users.noreply.github.com>
2025-06-23 11:56:16 +0800

    i added some exercise with dictionary

--- 1.py
+++ 1.py
@@ -7,3 +7,20 @@ hobbies = ["gaming", "watching youtube", "listening to music"]
 person = {"name":dave, "age":20}
 
 print(type(name), type(age), type(person), type(hobbies))
+
+
+
+#Build a small Python program that tracks basic data about your day, using at least 5 different data types.
+Daily_doings= {"name": "Dave", 
+               "Productive Hours": 2, 
+               "Hours of Sleep": 7.5, 
+               "Did you eat on time today?": False, 
+               "Your Top 3 Completed Task":[MULTILINE == "Mild Exercise", "Programming Block","Journalling"] 
+               } 
+
+print(Daily_doings)
+print(Daily_doings["Productive Hours"])
+print(Daily_doings["Did you eat on time today?"])
+print(Daily_doings["Hours of Sleep"])
+print(Daily_doings["name"])
+print(Daily_doings["Your Top 3 Completed Task"])
