51a742d789127c385d44a53e81efdd5dac488751
davetheloverboy <70562084+davetheloverboy@users.noreply.github.com>
2025-06-23 11:37:28 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+#Variable + Data Types
+name = "Dave" 
+age = 20
+height = "175.25cm"
+is_Student = False
+hobbies = ["gaming", "watching youtube", "listening to music"]
+person = {"name":dave, "age":20}
+
+print(type(name), type(age), type(person), type(hobbies))
