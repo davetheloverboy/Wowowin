9c341827df6390c3006db28f07a57b4501783f55
davetheloverboy <davejared1035@gmail.com>
2025-07-03 20:21:19 +0800

    completed the basic version. ill make another one that is better, has error handling. proper format putting things into fuctions and using them through a main func.

--- Grade_Evaluator.py
+++ Grade_Evaluator.py
@@ -2,36 +2,52 @@
 #ill use for loop instead of while loop
 
 #initializing the dictionary
+
+
+
+#V1
+
 ListOfGrades = { }
 
-#welcome message
+#welcome message and initialization
 grades_num = 0
 gradesSum = 0
 final_average = 0
 grades_num = input("Hi! How many grades do you want me to evaluate?: ")
 
+#getting the key value pairs of the subjects - grades
 for i in range(int(grades_num)): 
     subject_name = input("Input the name of the subject: ")
     subjectGrade = input("Input the grade of that subject: ")
 
     ListOfGrades[subject_name] = subjectGrade
 
+#printing the dictionary
 print("\nThese are your Grades\n")
 for key, value in ListOfGrades.items():
     print(f"{key} : {value}")
  
+#getting the sum of the grades
 for key,value in ListOfGrades.items():
     grade = int(value)
     gradesSum += grade
 
+#getting the average
 final_average = gradesSum / int(grades_num)
 print(f"Your average is {final_average}")
 
+#determining if the user passed or failed
 passing_grade = 75
-if final_average > passing_grade:
+print("The passing grade is 75 or higher.")
+if final_average >= passing_grade:
     print("You passed!")
+    print("Congratulations!")
 else:
     print("You failed")
+    print("Try harder next time")
+
+
+
 
 
 
