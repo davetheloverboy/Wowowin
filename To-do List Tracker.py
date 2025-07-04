75e3e6f66a54f349b6a3fc02556f5d1d27c0a533
davetheloverboy <davejared1035@gmail.com>
2025-07-04 22:04:49 +0800

    better version 1

--- To-do List Tracker.py
+++ To-do List Tracker.py
@@ -5,6 +5,7 @@
 #Version 1
 #intitialize the dictionary
 TaskList = { }
+numbers_Sequence = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 
 Inputted_task = ""
 while Inputted_task != "1":
@@ -14,7 +15,12 @@ while Inputted_task != "1":
 
 del TaskList["1"]
 print("These are your tasks: ")
-for key,value in TaskList.items():
-    print(f"{key}:{value}")
+Taskcount = len(TaskList) 
 
+for i,(key,value) in zip(numbers_Sequence,TaskList.items()):
+    print(f"{numbers_Sequence[i-1]}.{key}:{value}")
+
+
+
+#version 2
 
