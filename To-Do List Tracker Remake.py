55648537becd49692b255b494a4dd482edd12ae5
davetheloverboy <davejared1035@gmail.com>
2025-07-10 21:13:10 +0800

    yeeeeeey, it now works! ghahahah. made every end point have a return. also corrected my mistakes on task_marker. changed lowercase() in status checker with lower(). I made a ending main_menu call in the main menu function. not in every if elif choices. Nice oneee! I'll add the omni present program ender, and task_adder ensurer(I don't have the right words) next commit

--- To-Do List Tracker Remake.py
+++ To-Do List Tracker Remake.py
@@ -19,6 +19,7 @@ def task_getter(List_of_Tasks):
             List_of_Tasks[userInput] = "Not Done."
         except ValueError:
             print("Invalid Input.")
+    return
 
 #adds tasks based on user input
 def task_adder(List_of_Tasks):
@@ -33,12 +34,14 @@ def task_adder(List_of_Tasks):
                 break
         except ValueError:
             print("Invalid Input.")
+    return
 
 #prints the current tasks 
 def task_viewer(List_of_Tasks):
     print("These are your current tasks:")
     for index,(key,value) in enumerate(List_of_Tasks.items(),1):
         print(f"{index}. {key} : {value}")
+    return
 
 #deletes a chosen task by the user
 def task_deleter(List_of_Tasks):
@@ -48,6 +51,7 @@ def task_deleter(List_of_Tasks):
         del List_of_Tasks[userInput]
     else:
         print("Task not recorded.")
+    return
 
 #marks the chosen task as Done
 def task_marker(List_of_Tasks):
@@ -55,39 +59,49 @@ def task_marker(List_of_Tasks):
         try:
             userInput = input("What task do you want to mark done?: ")
             if userInput in List_of_Tasks:
+                print(f"{userInput} already in List_of_Tasks")
                 if List_of_Tasks[userInput] == "Done.":
-                    undoneTask = ("Task is already done. Mark undone? Yes or No:")
-                    if undoneTask.strip().lowercase() == "yes":
-                        List_of_Tasks[userInput] == "Done"
+                    undoneTask = input("Task is already done. Mark undone? Yes or No: ")
+                    if undoneTask.strip().lower() == "yes":
+                        List_of_Tasks[userInput] = "Not Done."
                         print(f"'{userInput}' marked as undone.")
                         return
-                    elif undoneTask.strip().lowercase() == "no":
-                        print("noted.")
+                    elif undoneTask.strip().lower() == "no":
+                        print(f"Noted. {userInput} still marked as done.")
                         return
-                else: 
-                    List_of_Tasks[userInput] == "Done."
+                elif List_of_Tasks[userInput] != "Done.": 
+                    List_of_Tasks[userInput] = "Done."
                     print(f"'{userInput}' marked as done.")
                     return
-            else:
+            elif userInput not in List_of_Tasks:
+                print("Task not in List_of_Tasks.")
                 print("There's no such task.")
+                return
         except ValueError:
             print("Invalid Input.")
+            return
+    
 
 #checks if the tasks are done or not done
 def status_checker(List_of_Tasks):
     task_done = 0
     task_not_done = 0
-    for value in List_of_Tasks.items():
-        if value.lowercase() == "Done":
+    for key,value in List_of_Tasks.items():
+        if value.lower() == "done.":
             task_done += 1
-        elif value.lower.case() == "Not Done":
+            print(f"{task_done} tasks done")
+        elif value.lower() == "not done.":
             task_not_done += 1
+            print(f"{task_not_done} tasks not done.")
         else:
             continue
     if task_done == len(List_of_Tasks):
         userStatus = 1
     else: 
         userStatus = 2
+    print(f"userStatus = {userStatus}")
+    print(f"task_done = {task_done}")
+    print(f"task_not_done = {task_not_done}")
     return userStatus,task_done,task_not_done
 
 #ends the program and prints ending messages
@@ -98,35 +112,36 @@ def program_ender(List_of_Tasks):
         print("Good Job! You finished all your tasks!")
     elif userStats == 2:
         print("Go finish your tasks!")
+        
 
 #the main controller, loops with itself and uses the other functions
 def main_menu(task_adder, task_viewer, task_marker, task_deleter, program_ender):
     userChoice = "0"
-    while userChoice != "5" or 5: 
+    while userChoice != "5": 
         userChoice = "0"
         print("What do you want to do?")
         menu_choices = {"Add task" : 1, "View Current Tasks" : 2, "Delete Task" : 3, "Mark a task Done": 4, "End the Program": 5 }
+        
+        #prints the choices
         for index,(key, value) in enumerate(menu_choices.items(),1):
             print(f"{index}. {key}. {value}")
+
+        #main_menu choices
         userChoice = input ("Press the corresponding number. ")
         if userChoice == "1":
             task_adder(List_of_Tasks)
-            main_menu(task_adder, task_viewer, task_marker, task_deleter,program_ender)
         elif userChoice == "2":
             task_viewer(List_of_Tasks)
-            main_menu(task_adder, task_viewer, task_marker, task_deleter,program_ender)
         elif userChoice == "3":
             task_deleter(List_of_Tasks)
-            main_menu(task_adder, task_viewer, task_deleter, task_marker, program_ender)
         elif userChoice == "4":
             task_marker(List_of_Tasks)
-            main_menu(task_adder, task_viewer, task_deleter, task_marker, program_ender)
+        elif userChoice == "5":
+            return
         else: 
             print("Invalid Input.")
-            main_menu(task_adder, task_viewer, task_deleter, task_marker, program_ender)
-    else:
-        print(" ")
-
+        main_menu(task_adder, task_viewer, task_deleter, task_marker, program_ender)
+        return
 
 #main function
 task_getter(List_of_Tasks)#gets the tasks
